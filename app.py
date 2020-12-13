9e095a0b20f5d1bf8da91cf84194baab33c09f37
Jia-Wei Yan <>
2020-12-14 02:03:47 +0800

    切分step.py

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 from flask import Flask,render_template,request
-import requests
-from bs4 import BeautifulSoup
+from step2 import result
 app = Flask(__name__)
 @app.route("/",methods=['POST','GET'])
 def index():
   if request.method =='POST':
     if request.values['send']=='送出':
-      re=requests.get('https://www.cwb.gov.tw/V8/C/L/Mountain/Mountain.html')
-      soup = BeautifulSoup(re.text, 'html.parser')
-      return render_template('index.html',name=soup.title.text+request.values['city'])
+      return render_template('index.html',name=result+request.values['city']+request.values['percent'])
   return render_template('index.html',name="")
 if __name__ == "__main__":
   app.run()
