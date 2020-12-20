45c160284b6d0ad235d344a480bbf1dc014e70e4
carol.yan <carol.yan@titansoft.com.sg>
2020-12-21 04:23:26 +0800

    串 文章

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask,render_template,request
 from step1 import result
-from step4 import article_result
+from step4 import searchArticle
 import json
 
 # citys
-with open('citys.json') as json_file:
+with open('citys.json',encoding="utf-8") as json_file:
   cityList=json.load(json_file)
 
 # chance of rain
@@ -12,15 +12,16 @@ percentList=[]
 for chance in range(0,110,10):
   percentList.append(chance)
 
-mountain="七星山"
-print(article_result)
+mountain="馬望曾呂山"
+articleList=searchArticle(mountain)
+print(searchArticle(mountain))
 
 app = Flask(__name__)
 
 @app.route("/",methods=['POST','GET'])
 def index():
   if request.method =='POST':
-    return render_template('index.html',name=result,citys=cityList,percents=percentList,city=request.values['city'],percent=request.values['percent'])
+    return render_template('index.html',name=result,citys=cityList,percents=percentList,city=request.values['city'],percent=request.values['percent'],articles=articleList)
   return render_template('index.html',name="",citys=cityList,percents=percentList)
 
 if __name__ == "__main__":
---
+++ step2.py
@@ -0,0 +1,48 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome('./chromedriver.exe')
+findpage = driver.get("https://www.cwb.gov.tw/V8/C/L/Mountain/Mountain.html")
+
+county = "臺北市"
+rain = 20
+
+n = 1
+mountain_lst=[]
+weather_dict={}
+check_lst=[]
+while True:
+    location = driver.find_element_by_id('CountySelect')
+    s1 = Select(location)
+    s1.select_by_visible_text(county)
+    mountain = driver.find_element_by_id('PID')
+    s2 = Select(mountain)
+    num = len(s2.options)-1
+    if n<=num:
+        search = s2.options[n].text
+        s2.select_by_visible_text(search)
+        start_search = driver.find_element_by_class_name("area_search_btn-v9")
+        start_search.click()
+        soup = BeautifulSoup(driver.page_source,"html.parser")
+
+        soup = soup.find("div",{"class":"col-md-12 margin-bottom-50 hidden-xs hidden-sm"},{"id":"PC_hr"})
+        temperature = soup.find("span",{"class":"tem-C is-active"},{"id":"GT_C_T"})
+        temp = temperature.text
+        chanceofrain = soup.find("tr",{"class":"rain_wrap hidden-xs hidden-sm"})
+        for chance in chanceofrain.findAll("td",{"colspan":"2"})[:3]:
+            answer = chance.text.replace("%","")
+            answer = int(answer)
+            check_lst.append(answer)
+        check_lst.sort(reverse=True)
+        if check_lst[0]<=rain:
+            mountain_lst.append(search)
+            weather_dict[search] = temp
+        n+=1
+        check_lst=[]
+    elif n>num:
+        break
+driver.close()
