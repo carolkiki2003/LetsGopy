961a44c43ecdbf0ca95a699325c688c7edfecd53
Jia-Wei Yan <>
2020-12-22 02:57:25 +0800

    no message

--- app.py
+++ app.py
@@ -4,16 +4,17 @@ from step1 import result
 # from step4 import searchArticle
 # import json
 
+app = Flask(__name__)
+
 # citys
 # with open('./citys.json',encoding="utf-8") as json_file:
 #   cityList=json.load(json_file)
-# cityList=["臺北市","新北市","桃園市","新竹市","新竹縣","苗栗縣","臺中市","南投縣","嘉義縣","高雄市","屏東縣","宜蘭縣","花蓮縣"]
+cityList=["臺北市","新北市","桃園市","新竹市","新竹縣","苗栗縣","臺中市","南投縣","嘉義縣","高雄市","屏東縣","宜蘭縣","花蓮縣"]
+
 # chance of rain
-# percentList=[0,1,2]
+percentList=[0,1,2]
 # for chance in range(0,110,10):
 #   percentList.append(chance)
-
-app = Flask(__name__)
 
 @app.route("/",methods=['POST','GET'])
 def index():
